25a4ae9f18fddfa9184e301cd80ec146cefef999
forever <user@forever>
2026-04-14 00:53:55 +0300

    add SecurityConfig, SecurityTests for payment-service

--- payment/src/test/java/ru/ugaforever/reactive/market/payment/server/security/SecurityBalanceControllerTest.java
+++ payment/src/test/java/ru/ugaforever/reactive/market/payment/server/security/SecurityBalanceControllerTest.java
@@ -1,13 +1,13 @@
-package ru.ugaforever.reactive.market.payment.server.api;
+package ru.ugaforever.reactive.market.payment.server.security;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
+import ru.ugaforever.reactive.market.payment.server.api.BalanceController;
 import ru.ugaforever.reactive.market.payment.server.domain.BalanceResponse;
 import ru.ugaforever.reactive.market.payment.server.service.BalanceService;
 
@@ -16,7 +16,7 @@ import java.math.BigDecimal;
 import static org.mockito.Mockito.when;
 
 @WebFluxTest(BalanceController.class)
-class BalanceControllerTest {
+public class SecurityBalanceControllerTest {
 
     @Autowired
     private WebTestClient webTestClient;
@@ -25,9 +25,7 @@ class BalanceControllerTest {
     private BalanceService balanceService;
 
     @Test
-    @WithMockUser(authorities = "SERVICE")
-    void getBalance_WhenAccountExists_ShouldReturnBalance() {
-
+    void shouldReturn401_WhenNoTokenProvided() {
         BigDecimal accountId = new BigDecimal(12345);
         BalanceResponse expectedResponse = new BalanceResponse();
         expectedResponse.setBalance(accountId);
@@ -40,8 +38,24 @@ class BalanceControllerTest {
         webTestClient.get()
                 .uri("/api/v1/balance/{accountId}", accountId.toString())
                 .exchange()
-                .expectStatus().isOk()
-                .expectBody(BalanceResponse.class)
-                .isEqualTo(expectedResponse);
+                .expectStatus().isUnauthorized();
+    }
+
+    @Test
+    @WithMockUser(authorities = "SERVICE")
+    void shouldReturnMessage_WhenHasServiceRole() {
+        BigDecimal accountId = new BigDecimal(12345);
+        BalanceResponse expectedResponse = new BalanceResponse();
+        expectedResponse.setBalance(accountId);
+        expectedResponse.setBalance(new BigDecimal(1000.50));
+        expectedResponse.setCurrency("RUB");
+
+        when(balanceService.getBalance(accountId.toString()))
+                .thenReturn(Mono.just(expectedResponse));
+
+        webTestClient.get()
+                .uri("/api/v1/balance/{accountId}", accountId.toString())
+                .exchange()
+                .expectStatus().isOk();
     }
 }
